698138632daf41390e66edf5f56e82c2fae1cce5
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-01 16:12:37 -0600

    context defined

--- elecciones.py
+++ elecciones.py
@@ -1,6 +1,9 @@
 from pyspark import SparkContext
 from pyspark import SparkConf
 
+APP_NAME = 'elecciones'
+conf = SparkConf().setAppName(APP_NAME)
+
 sc = SparkContext(conf=conf)
 registro = sc.textFile('hdfs:///user/root/hdfsdirectory/nacional.csv')
 print registro.first()
