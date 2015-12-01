7bc69f379efea0b1bcdeaf17356322cdba43ab59
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-01 16:10:44 -0600

    import sc

--- elecciones.py
+++ elecciones.py
@@ -1,3 +1,6 @@
+from pyspark import SparkContext
+from pyspark import SparkConf
+
 sc = SparkContext(conf=conf)
 registro = sc.textFile('hdfs:///user/root/hdfsdirectory/nacional.csv')
 print registro.first()
