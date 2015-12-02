08775cdb6c81ce116b9fe54deab68b55be26c505
Alejandro J. Rojas <ale@trixandtrax.com>
2015-12-02 07:46:57 -0600

    update print

--- elecciones.py
+++ elecciones.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext
 from pyspark import SparkConf
+from pprint import pprint
 
 APP_NAME = 'elecciones'
 conf = SparkConf().setAppName(APP_NAME)
@@ -7,5 +8,9 @@ conf = SparkConf().setAppName(APP_NAME)
 sc = SparkContext(conf=conf)
 registro = sc.textFile('hdfs:///data/elecciones/nacional.csv')
 centros = sc.textFile('hdfs:///data/elecciones/centros.csv')
-print registro.first()
-print centros.take(2)
+ama = sc.textFile('hdfs:///data/elecciones/AMA.csv')
+pprint (registro.first())
+print '-----------------------------------------------------'
+pprint (centros.take(2))
+print '-----------------------------------------------------'
+pprint (ama.take(2))
